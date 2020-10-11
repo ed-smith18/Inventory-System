8f3987e4225799e2355cf8c8f3dd842489d2aed4
Edward Smith <ed18nsxr@gmail.com>
2020-10-10 20:09:58 -0400

    Adding support for tabs

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 console.log("JavaScript is loading properly")
+
+
+const tabs = document.querySelectorAll('[data-tab-target]')
+const tabContents = document.querySelectorAll('[data-tab-content]')
+
+tabs.forEach(tab => {
+    tab.addEventListener('click', () => {
+        const target = document.querySelector(tab.dataset.tabTarget)
+        tabContents.forEach(tabContent => {
+            tabContent.classList.remove('active');
+        })
+        target.classList.add('active');
+    })
+})
+
+
+// //Dom Elements
+const darkButton = document.getElementById('Dark Mode');
+
+darkButton.onclick = () => {
+
+};
